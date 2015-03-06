dd5e11fdc7a8d0fdd5f27dea25b3e711d4838c6a
Ben Kutil <ben@make-things.com>
2015-03-06 14:54:06 -0500

    update campaign ideas to use curl

--- src/www/content/themes/jhu-idealab/functions/ideascale-campaign_ideas.php
+++ src/www/content/themes/jhu-idealab/functions/ideascale-campaign_ideas.php
@@ -1,22 +1,38 @@
 <?php
 function ideaScaleIdeas($id) {
-    $title      = apply_filters('widget_title', $instance['title']);
-    $community_url  = 'https://myidealab.jhu.edu';
-    $api_key    = 'd3a96aed-3e5f-466c-a7a0-f7600ca57515';
+  $ch = curl_init();
+  $title      = apply_filters('widget_title', $instance['title']);
+  $community_url  = 'https://myidealab.jhu.edu';
+  $api_key    = 'd3a96aed-3e5f-466c-a7a0-f7600ca57515';
 
-    $opts = array(
-      'http'=>array(
-        'method'=>"GET",
-        'header'=>"Accept-language: en\r\n" .
-                  "Cookie: foo=bar\r\n" .
-                  "api_token: ".$api_key."\r\n".
-                  "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31\r\n"
-      )
-    );
-    $context = stream_context_create($opts);
-    // Open the file using the HTTP headers set above
-    $file = file_get_contents($community_url . '/a/rest/v1/campaigns/' . $id . '/ideas/top', false, $context);
-    $data = json_decode($file);
+  // Set url
+  curl_setopt($ch, CURLOPT_URL, $community_url . '/a/rest/v1/campaigns/' . $id . '/ideas/top');
 
-    return $data;
+  // Set method
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+  // Set options
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+  // Set headers
+  curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31",
+    "api_token: " . $api_key,
+    "Accept-language: en",
+   ]
+  );
+
+  // Send the request & save response to $resp
+  $resp = curl_exec($ch);
+
+  if(!$resp) {
+    die('Error: "' . curl_error($ch) . '" - Code: ' . curl_errno($ch));
+  } else {
+    $data = json_decode($resp);
+  }
+
+  // Close request to clear up some resources
+  curl_close($ch);
+
+  return $data;
 }
