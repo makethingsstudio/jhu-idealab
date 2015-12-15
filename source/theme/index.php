3c5342b350029a1c6d81dd441a43055c020a0197
Ben Kutil <ben@make-things.com>
2015-12-15 16:08:07 -0500

    :zap: update challenge statement field

--- source/theme/index.php
+++ source/theme/index.php
@@ -6,6 +6,7 @@ $context['post'] = $post;
 $context['info_title'] = get_field('area_title', 'options');
 $context['info_content'] = get_field('area_content', 'options');
 $context['challenges'] = get_field('challenges', 'options');
+$context['challenge_statement'] = get_field('challenge_statement', 'options');
 //$context['challenges'] = json_cached_api_results(ideaScaleCampaigns());
 $context['html_class'] = 'l-interior';
 $context['masthead_class'] = 'masthead--interior';
