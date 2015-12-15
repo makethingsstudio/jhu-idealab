3362c81619df4d743869481c5126f99745e57f0a
Ben Kutil <ben@make-things.com>
2015-12-15 15:58:43 -0500

    :zap: remove plugins and theme from generated files

--- src/www/content/themes/jhu-idealab/functions/menus.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function add_to_context($data){
-    /* So here you are adding data to Timber's context object, i.e... */
-    /* Now, in similar fashion, you add a Timber menu and send it along to the context. */
-    $data['primary'] = new TimberMenu('primary'); // This is where you can also send a Wordpress menu slug or ID
-    return $data;
-}
