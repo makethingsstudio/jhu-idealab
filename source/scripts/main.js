b595fee0851a322d447240317263d87e01904846
Ben Kutil <ben@make-things.com>
2016-02-04 11:10:14 -0500

    :zap: update format of js

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -4,62 +4,61 @@
 var library;
 var CHALLENGES;
 
+function library( module ) {
+    $( function () {
+        if ( module.init ) {
+            // module.init();
+        }
+    } );
 
-function library(module) {
-  $(function () {
-    if (module.init) {
-      // module.init();
-    }
-  });
-
-  return module;
+    return module;
 }
 
+CHALLENGES = ( library( function ( $ ) {
+    var apiUrl = 'https://jhu.ideascale.com/a/rest/v1';
+    var apiKey = 'd3a96aed-3e5f-466c-a7a0-f7600ca57515';
+    var campaignEndpoint = apiUrl + '/campaigns';
+    var settings;
+    var initChallenges;
+    var bindUi;
+    var getData;
 
+    settings = {
+        campaigns: {}
+    };
 
-CHALLENGES = (library(function ($) {
-  var apiUrl = 'https://jhu.ideascale.com/a/rest/v1';
-  var apiKey = 'd3a96aed-3e5f-466c-a7a0-f7600ca57515';
-  var campaignEndpoint = apiUrl + '/campaigns';
-  var settings;
-  var initChallenges;
-  var bindUi;
-  var getData;
+    bindUi = function () {
+        console.log( $( 'body' ) );
+    };
 
-  settings = {
-    campaigns: {}
-  };
+    initChallenges = function () {
+        bindUi();
+    };
 
-  bindUi = function () {
-    console.log($('body'));
-  };
+    getData = function () {
+        $.ajax( {
+            type: 'GET',
+            url: campaignEndpoint,
+            headers: {
+                'api_key': apiKey
+            },
+            dataType: 'jsonp',
+            success: function ( data ) {
+                // Supposing this JSON payload was received:
+                //   {"project": {"id": 42, "html": "<div>..." }}
+                // append the HTML to context object.
+                console.log( data );
+            },
+            error: function ( xhr, type ) {
+                console.log( xhr, type,
+                    'info' );
+            }
+        } );
+    };
 
-  initChallenges = function () {
-    bindUi();
-  };
+    return {
+        options: settings,
+        init: initChallenges()
+    };
+} )( Zepto ) );
 
-  getData = function () {
-    $.ajax({
-      type: 'GET',
-      url: campaignEndpoint,
-      headers: {
-        'api_key': apiKey
-      },
-      dataType: 'jsonp',
-      success: function(data){
-          // Supposing this JSON payload was received:
-          //   {"project": {"id": 42, "html": "<div>..." }}
-          // append the HTML to context object.
-          console.log(data);
-        },
-        error: function(xhr, type){
-          console.log(xhr, type, 'info');
-        }
-    });
-  };
-
-  return {
-    options: settings,
-    init: initChallenges()
-  };
-})(Zepto));
